f36323d7680dbdb3b44af100a099da45b2a574be
sam-muntean <sam.muntean@gmail.com>
2018-02-26 19:09:56 +0200

    Books and authors read

--- domain/models.go
+++ domain/models.go
@@ -1,17 +1,36 @@
 package domain
 
+import (
+	"fmt"
+	//"golang.org/x/tools/go/gcimporter15/testdata"
+)
+
 type AuthorDto struct {
-	UUID      string
-	FirstName string
-	LastName  string
-	Birthday  string
-	Death     string
+	UUID      string `json:"uuid"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Birthday  string `json:"birthday"`
+	Death     string `json:"death"`
 }
 
 type BookDto struct {
-	UUID        string
-	Title       string
-	NoPages     int
-	ReleaseDate string
+	UUID        string `json:"uuid"`
+	Title       string `json:"title"`
+	NoPages     int `json:"noPages"`
+	ReleaseDate string `json:"releaseDate"`
 	Author      AuthorDto
 }
+
+func (b BookDto) String() string {
+	return fmt.Sprintf("BookDtp{UUID=%s, Title=%s, NoPages=%d, ReleaseDate=%s, Author=%s}",
+		b.UUID, b.Title, b.NoPages, b.ReleaseDate, b.Author)
+}
+
+func (a AuthorDto) String() string {
+	return fmt.Sprintf("AuthorDto{UUID=%s, FirstName=%s, LastName=%s, Birthday=%s, Death=%s}",
+		a.UUID, a.FirstName, a.LastName, a.Birthday, a.Death)
+}
+
+var Books []BookDto
+
+var Authors []AuthorDto
--- main/main.go
+++ main/main.go
@@ -8,20 +8,43 @@ import (
 )
 
 func main() {
-	fileC, err := ioutil.ReadFile("main/books.json")
+	fileC, err := ioutil.ReadFile("main/authors.json")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(fileC))
-	var books []domain.BookDto
+	//fmt.Println(string(fileC))
 
-	if err = json.Unmarshal(fileC, &books); err != nil {
+	if err = json.Unmarshal(fileC, &domain.Authors); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("the authors are: ")
+	fmt.Println(domain.Authors)
+
+	fileC, err = ioutil.ReadFile("main/books.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println(string(fileC))
+
+	if err = json.Unmarshal(fileC, &domain.Books); err != nil {
 		panic(err)
 	}
 
 	fmt.Println("the books are: ")
-	fmt.Println(books)
+	fmt.Println(domain.Books)
+
+	//serializedData, err := json.Marshal(books)
+	//if err != nil{
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("ser books are:")
+	//fmt.Println(string(serializedData))
+
 
 }
